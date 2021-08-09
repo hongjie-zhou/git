b2e812f50956dd9020b384df7cb809d5fdb55fd3
zhj <2414680855@qq.com>
2021-08-10 06:50:43 +0800

    GitCommit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,9 +3,6 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hellow git");
-        System.out.println("hello");
-        System.out.println("分支");
-        System.out.println("再次修改");
+
     }
 }
